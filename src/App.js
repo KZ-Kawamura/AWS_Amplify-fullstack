02121ea9518aea64ad0ec4569f589722efff2d1a
DESKTOP-UHR58M5\kzsol <kzsolutionconsulting@gmail.com>
2024-08-23 18:25:46 +0900

    delet Hello

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello React</h1>
         <h1>Hello React</h1>
-        <h1>Hello React</h1>
-        <h1>Hello React</h1>
-        <h1>Hello React</h1>
-        <h1>Hello React</h1>
       </header>
     </div>
   );
